d7f6c8398b1b042bc4eeb4dc119b4fd32c888f79
Frudd <giadi735@gmail.com>
2023-01-19 01:24:52 +0100

    finished gameEngine

--- src/modules/gameEngine.ts
+++ src/modules/gameEngine.ts
@@ -1,8 +1,7 @@
-import { Console } from 'console'
-import type { Card, PlayerCards } from './PlayerCards'
+import { Card, defaults, PlayerCards } from './PlayerCards.js'
 
 export class Engine {
-  players: {
+  playersCards: {
     playerOne: PlayerCards
     playerTwo: PlayerCards
     playerThree: PlayerCards
@@ -14,19 +13,36 @@ export class Engine {
    * @returns {object}
    */
   constructor() {
-    this.players = {
-      playerOne: [],
-      playerTwo: [],
-      playerThree: [],
-      playerFour: [],
-    }
+    const deck = this.createDeck()
+    const shuffledDeck = this.shuffleDeck(deck)
+    const playerCards = this.dealCards(shuffledDeck)
+
+    this.playersCards = playerCards
   }
 
-  addCard(card: Card) {
-    this.players.playerOne.push(card)
+  createDeck(): PlayerCards {
+    return defaults.values.flatMap((value) => defaults.suits.map((suit) => ({ value, suit })))
+  }
+
+  shuffleDeck(deck: PlayerCards): PlayerCards {
+    const shuffledDeck = [...deck]
+    for (let i = shuffledDeck.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1))
+      ;[shuffledDeck[i]!, shuffledDeck[j]!] = [shuffledDeck[j]!, shuffledDeck[i]!]
+    }
+    return shuffledDeck
+  }
+
+  dealCards(deck: PlayerCards) {
+    const deckCopy = [...deck]
+    const playerOne: PlayerCards = deckCopy.slice(0, 13)
+    const playerTwo: PlayerCards = deckCopy.slice(13, 26)
+    const playerThree: PlayerCards = deckCopy.slice(26, 39)
+    const playerFour: PlayerCards = deckCopy.slice(39)
+    return { playerOne, playerTwo, playerThree, playerFour }
   }
 }
 
 const engine = new Engine()
 
-console.log(engine)
+console.log(engine.playersCards)
